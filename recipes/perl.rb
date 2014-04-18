e3365fdc36b41376172436491526fc6399e1ec57
Simon Wingrove <simon.wingrove@metaswitch.com>
2014-04-18 00:27:22 +0000

    Add the asciiquarium

---
+++ recipes/perl.rb
@@ -0,0 +1,19 @@
+package "cpanminus"
+execute "Installing Perl modules..." do
+	command "cpanm --sudo Term::Animation"
+  action :run
+end
+
+execute "Install the asciiquarium" do
+  user node["username"]
+  command "curl -L https://raw.githubusercontent.com/cmatsuoka/asciiquarium/master/asciiquarium > " + node["home_dir"] + "/bin/asciiquarium"
+  creates node["home_dir"] + "bin/asciiquarium"
+  actions :run
+  notifies :touch, "file[" + node["home_dir"] + "bin/asciiquarium]"
+end
+
+file node["home_dir"] + "bin/asciiquarium" do
+  action :nothing
+  owner node["username"]
+  mode 0755
+end
